a867ac44a26d07f95dc5f5e2bec0017fa7282645
Kuts, Andrey <a.kuts@itransition.com>
2019-01-02 03:12:52 +0300

    Added DownloadingService.framework
    Andre Kuts

---
+++ DownloadingService.framework/Headers/DownloadingService.h
@@ -0,0 +1,18 @@
+//
+//  DownloadingService.h
+//  DownloadingService
+//
+//  Created by Andre on 12/29/18.
+//  Copyright © 2018 Free. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DownloadingService.
+FOUNDATION_EXPORT double DownloadingServiceVersionNumber;
+
+//! Project version string for DownloadingService.
+FOUNDATION_EXPORT const unsigned char DownloadingServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownloadingService/PublicHeader.h>
+
